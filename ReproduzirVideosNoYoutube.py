7c8cb509da1eceac5a94b49810a93f102691da37
VitorPL07 <62154573+VitorPL07@users.noreply.github.com>
2020-05-19 08:49:58 -0300

    Update ReproduzirVideosNoYoutube.py

--- ReproduzirVideosNoYoutube.py
+++ ReproduzirVideosNoYoutube.py
@@ -1,24 +1,32 @@
 from youtube_search import YoutubeSearch
 import webbrowser as web
 
+# Pegar a pesquisa desejada
 pg = input("Digite sua pesquisa: ")
 
+# Criar listas para armazenar os dados de casa resultado
 nome = []
 links = []
 identificacao = []
 
+# Rodas a lib com a pesquisa digitada
 pesquisa = YoutubeSearch(pg, max_results=4).to_dict()
 
+# Armazenar os resultados da pesquisa nas listas correspondentes
 for c in pesquisa:
 	nome.append(c["title"])
 	links.append(c["link"])
 	
+# Colocar uma maneira de escolher qual vídeo deseja ser aberto
 for i in range(len(nome)):
 	identificacao.append(i + 1)
+
+# Apresentar os resultados de maneira intuitiva
 print("CÓDIGO    //    TITULO")
 for a in range(len(nome)):
 	print(f"\n[ \033[1;34m{identificacao[a]}\033[m ] - {nome[a]}")
 
+# Criar um loop e só quebrar caso o usuário escolha uma opção válida
 while True:
 	try:
 		abrir = int(input("\nDigite o código referente ao vídeo que deseja abrir: "))
